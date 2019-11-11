f283c601b7b17339d530b5cc4f746fe62beb5fde
Tim Vrakas <tim.vrakas@gmail.com>
2019-11-10 20:57:24 -0800

    orks

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include <Servo.h>
 
 Adafruit_NeoPixel strip(8, GPIO_NUM_23, NEO_GRB + NEO_KHZ800);
 
 uint8_t color = 0;
 
+Servo servo1;
+
 void setup() {
-  pinMode(GPIO_NUM_2,OUTPUT);
   strip.begin(); // Initialize NeoPixel strip object (REQUIRED)
   strip.show();  // Initialize all pixels to 'off'
+  servo1.attach(GPIO_NUM_22);
 }
 
 void loop() {
@@ -17,4 +20,5 @@ void loop() {
   strip.fill(strip.ColorHSV(color*256));
   strip.show();
   delay(100);
+  servo1.write(color);
 }
